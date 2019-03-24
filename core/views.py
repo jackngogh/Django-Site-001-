25b6a6ac2b3ce68d9e16ba923414ca06830e5338
Jack Ngo <jngo@example.com>
2019-03-24 11:51:47 -0400

    routes

--- core/views.py
+++ core/views.py
@@ -1,4 +1,25 @@
 from django.shortcuts import render
 
+
 def index(request):
-    return render(request, 'index.html', {})
+    context = {
+        "title": "Welcome",
+        "header": "Home Index"
+    }
+    return render(request, 'index.html', context)
+
+
+def about(request):
+    context = {
+        "title": "About us",
+        "header": "About us"
+    }
+    return render(request, 'index.html', context)
+
+
+def contact(request):
+    context = {
+        "title": "Contact us",
+        "header": "Contact us"
+    }
+    return render(request, 'index.html', context)
