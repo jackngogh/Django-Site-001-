0c7410e93e43f9700a120de8c50eeea093871a5b
Jack Ngo <jngo@example.com>
2019-04-13 12:02:51 -0400

    separate template

--- core/views.py
+++ core/views.py
@@ -2,24 +2,15 @@ from django.shortcuts import render
 
 
 def index(request):
-    context = {
-        "title": "Welcome",
-        "header": "Home Index"
-    }
+    context = {}
     return render(request, 'index.html', context)
 
 
 def about(request):
-    context = {
-        "title": "About us",
-        "header": "About us"
-    }
-    return render(request, 'index.html', context)
+    context = {}
+    return render(request, 'about.html', context)
 
 
 def contact(request):
-    context = {
-        "title": "Contact us",
-        "header": "Contact us"
-    }
-    return render(request, 'index.html', context)
+    context = {}
+    return render(request, 'contact.html', context)
