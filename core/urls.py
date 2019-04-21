51c3657acf8d6f257a9627806410f40681de9e13
Jack Ngo <jngo@example.com>
2019-04-21 12:04:10 -0400

    static files

--- core/urls.py
+++ core/urls.py
@@ -1,3 +1,7 @@
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
+from django.conf.urls.static import static
+
 from django.urls import path
 from .views import index, about, contact
 
@@ -6,3 +10,5 @@ urlpatterns = [
     path('about', about, name='about'),
     path('contact', contact, name='contact'),
 ]
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
