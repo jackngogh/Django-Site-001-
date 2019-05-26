6530108b38f67379e8b1e1becf665e575d7552de
Jack Ngo <jngo@example.com>
2019-05-26 12:04:45 -0400

    logout works

--- core/urls.py
+++ core/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.views import LogoutView
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf import settings
 from django.conf.urls.static import static
@@ -9,6 +10,8 @@ urlpatterns = [
     path('', index, name='index'),
     path('about', about, name='about'),
     path('contact', contact, name='contact'),
+    path('logout', LogoutView.as_view(
+        template_name='logout.html'), name='logout'),
 ]
 urlpatterns += staticfiles_urlpatterns()
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
