a6dbe616056effdc1fb6881822b84090dd5f8598
Jack Ngo <jngo@example.com>
2019-06-09 11:47:59 -0400

    Add login page

--- core/urls.py
+++ core/urls.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.views import LogoutView
+from django.contrib.auth.views import LogoutView, LoginView
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf import settings
 from django.conf.urls.static import static
@@ -12,6 +12,7 @@ urlpatterns = [
     path('contact', contact, name='contact'),
     path('logout', LogoutView.as_view(
         template_name='logout.html'), name='logout'),
+        path('login', LoginView.as_view(template_name='login.html'), name='login')
 ]
 urlpatterns += staticfiles_urlpatterns()
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
